42618a0891325800148808e5f044fa39f2d79a8c
Harris-Ricardo <119436182+Harris-Ricardo@users.noreply.github.com>
2023-06-21 21:27:20 -0500

    Delete Business website directory

--- Business website/T and C.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var checkbox = document.getElementById('checkbox');
-  var acceptButton = document.getElementById('acceptButton');
-
-  checkbox.addEventListener('change', function() {
-      acceptButton.disabled = !checkbox.checked;
-  });
-});
